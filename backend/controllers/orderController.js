5f44776da3ae6ac5705c1be50235dd46e4940a43
Kirill-Ryzhkov <slusc10a@gmail.com>
2024-09-10 23:59:05 -0400

    backend_add_decreasing_balance

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,10 +1,23 @@
 const Order = require("../models/Order");
+const User = require("../models/User");
 
 const createOrder = async (req, res) => {
     const { name } = req.body;
 
     try {
-        const newOrder = await Order.createOrder(name, req.user._id);
+
+        const user = await User.findOne({_id: req.user._id});
+        let newOrder;
+        
+        if (user.balance >= 500) {
+            newOrder = await Order.createOrder(name, req.user._id);
+            user.balance -= 500;
+            user.save();
+        } else {
+            //stripe stuff
+
+            newOrder = 'nothing';
+        }
         res.status(200).json({newOrder});
     } catch (error) {
         res.status(401).json({error: error.message});
