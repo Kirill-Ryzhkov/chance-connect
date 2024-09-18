32721a43461e08427d3c7d53e82f12ba6b9a8bc5
Kirill-Ryzhkov <slusc10a@gmail.com>
2024-09-17 22:41:16 -0400

    Create stuff for admin cafe

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -2,16 +2,23 @@
 const express = require("express");
 const requireAuth = require("../middleware/requireAuth");
 const isAdminRole = require("../middleware/isAdminRole");
+const corsHeader = require("../middleware/corsHeaders");
 const {
     createEvent,
-    toggleCafeEvent
+    toggleCafeEvent,
+    statusCafe
 } = require("../controllers/eventController");
 
 const router = express.Router();
 
 // home example
+router.use(corsHeader);
+
+router.get("/statusCafe/:name", statusCafe);
+
 router.use(requireAuth);
 router.use(isAdminRole);
+
 router.post("/", createEvent);
 router.get("/toggleCafe/:name", toggleCafeEvent);
 
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,16 +2,18 @@ const express = require("express");
 const corsHeader = require("../middleware/corsHeaders");
 const requireAuth = require("../middleware/requireAuth");
 
-const { createOrder, paymentOrder } = require("../controllers/orderController");
+const { createOrder, getOrders, completeOrder, paymentOrder } = require("../controllers/orderController");
 
 const router = express.Router();
 
 router.use(corsHeader);
 
-router.post("/payment", paymentOrder );
+router.post("/payment", paymentOrder);
 
 router.use(requireAuth);
 
 router.post("/", createOrder);
+router.get("/", getOrders);
+router.post("/complete", completeOrder);
 
 module.exports = router;
